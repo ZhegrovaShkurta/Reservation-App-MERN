c68be6b5387533e50a4b568462142b4d64bdef99
ZhegrovaShkurta <shzhegrova1@gmail.com>
2024-07-28 15:42:13 +0200

    commit

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -1,4 +1,6 @@
 import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+
 
 export const createHotel = async (req,res,next)=>{
     const newHotel = new Hotel(req.body)
@@ -41,12 +43,57 @@ export const getHotel = async (req,res,next)=>{
              next(err);
           }
 }
+export const getHotels = async (req, res, next) => {
+    const { min = '1', max = '999', limit = '10', ...others } = req.query;
+    try {
+        const minPrice = Number(min);
+        const maxPrice = Number(max);
+        const resultLimit = Number(limit);
 
-export const getHotels = async (req,res,next)=>{
-    try{
-        const hotels = await Hotel.find();
-            res.status(200).json(hotels);
-          }catch(err){
-              next(err);
+        if (isNaN(minPrice) || isNaN(maxPrice) || isNaN(resultLimit)) {
+            return res.status(400).json({ message: "Invalid query parameters" });
+        }
+
+        const hotels = await Hotel.find({
+            ...others,
+            cheapestPrice: { $gt: minPrice, $lt: maxPrice },
+        }).limit(resultLimit);
+
+        res.status(200).json(hotels);
+    } catch (err) {
+        next(err);
+    }
+};
+
+      export const countByCity = async (req,res,next)=>{
+        const cities = req.query.cities.split(",")
+        try{
+            const list = await Promise.all(cities.map(city=>{
+                return Hotel.countDocuments({city:city})
+               }))
+                res.status(200).json(list);
+              }catch(err){
+                  next(err);
+              }
           }
-      }
+
+          export const countByType = async (req,res,next)=>{
+            try{
+                const hotelCount = await Hotel.countDocuments({type:"hotel"});
+                const apartmentCount = await Hotel.countDocuments({type:"apartment"});
+                const resortCount = await Hotel.countDocuments({type:"resort"});
+                const villaCount = await Hotel.countDocuments({type:"villa"});
+                const cabinCount = await Hotel.countDocuments({type:"cabin"});
+                  
+                res.status(200).json([
+                  { type: "hotel", count: hotelCount },
+                  { type: "apartments", count: apartmentCount },
+                  { type: "resorts", count: resortCount },
+                  { type: "villas", count: villaCount },
+                  { type: "cabins", count: cabinCount },
+                ]);
+                  }catch(err){
+                      next(err);
+                  }
+              }
+    
